fe0e5d6dd392b6903ebbf0345b359f67f713ecb1
Ye lin aung <hello.yelinaung@gmail.com>
2016-12-04 15:20:13 +0630

    ADded Anko View"

--- app/src/main/java/io/github/rin_da/ucsynews/presentation/login/view/LoginView.kt
+++ app/src/main/java/io/github/rin_da/ucsynews/presentation/login/view/LoginView.kt
@@ -1,5 +1,6 @@
 package io.github.rin_da.ucsynews.presentation.login.view
 
+import android.content.res.ColorStateList
 import android.graphics.Color
 import android.view.View
 import com.pawegio.kandroid.dp
@@ -7,20 +8,28 @@ import io.github.rin_da.ucsynews.presentation.abstract.view.AbstractView
 import io.github.rin_da.ucsynews.presentation.base.model.BaseModel
 import io.github.rin_da.ucsynews.presentation.base.ui.fontAwesome
 import io.github.rin_da.ucsynews.presentation.login.activity.LoginActivity
-import org.jetbrains.anko.AnkoComponent
-import org.jetbrains.anko.AnkoContext
-import org.jetbrains.anko.centerHorizontally
-import org.jetbrains.anko.relativeLayout
+import org.jetbrains.anko.*
+import org.jetbrains.anko.cardview.v7.cardView
 
 /**
  * Created by user on 12/4/16.
  */
 
 class LoginView : AbstractView<BaseModel>(), AnkoComponent<LoginActivity> {
+    private val USER_VIEW_ID = 1
+    var states = arrayOf(intArrayOf(android.R.attr.state_enabled), // enabled
+            intArrayOf(-android.R.attr.state_enabled), // disabled
+            intArrayOf(-android.R.attr.state_checked), // unchecked
+            intArrayOf(android.R.attr.state_pressed)  // pressed
+    )
+
+    var colors = intArrayOf(Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE)
+
     override fun createView(ui: AnkoContext<LoginActivity>): View {
         with(ui) {
             relativeLayout {
-                fontAwesome {
+                val user_view = fontAwesome {
+                    id = USER_VIEW_ID
                     setBackgroundColor(Color.parseColor("#5bffffff"))
                     setRadius(100)
                     setFontIconSize(60);
@@ -28,6 +37,16 @@ class LoginView : AbstractView<BaseModel>(), AnkoComponent<LoginActivity> {
                 }.lparams(width = dp(100), height = dp(100)) {
                     centerHorizontally()
                     topMargin = dp(80)
+                }
+                val card = cardView {
+                    cardBackgroundColor = ColorStateList(states, colors)
+                    verticalLayout {
+
+                    }
+                }.lparams(width = dp(300), height = dp(375)) {
+                    below(USER_VIEW_ID)
+                    centerHorizontally()
+                    topMargin = dp(25)
                 }
             }
         }
